01e364fdc77d1fb043512aa0641fd060027b1d82
Trung Truong <trung.truong@actian.com>
2022-04-14 08:38:38 -0500

    blah

---
+++ s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "b" {
+  bucket = "tt-harness-poc-bucket-dev"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+terraform {
+  required_version = ">= 1.0.1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2
+}
